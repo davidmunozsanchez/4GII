8550230e949270a522588a516b9d9a4f04dc9a5c
davidMS <dmunozs14@correo.ugr.es>
2022-09-20 17:49:26 +0200

    Modificaciones P0 IG

--- IG/PRACTICAS/Práctica_0/practica_0.cpp
+++ IG/PRACTICAS/Práctica_0/practica_0.cpp
@@ -6,107 +6,109 @@
 #include <GL/glu.h>
 #include <GL/freeglut.h>
 
-
-
-void Ejes (int width)
-{   
-    glLineWidth(width);
-    glBegin(GL_LINES);
-       glColor3f(1.0,0.0,0.0);
-       glVertex3f(-1.0,0.0,0.0);
-       glVertex3f(1.0,0.0,0.0);
-       glColor3f(0.0,1.0,0.0);
-       glVertex3f(0.0,-1.0,0.0);
-       glColor3f(1.0,1.0,0.0); //En modo flat el eje se queda en amarillo, para que se vea en verde y amarillo hay que poner GL_SMOOTH. FLAT coge el último color activado
-       glVertex3f(0.0,1.0,0.0);   
-    glEnd();
-   
-       
+void Ejes(int width)
+{
+   glLineWidth(width);
+   glBegin(GL_LINES);
+   glColor3f(1.0, 0.0, 0.0);
+   glVertex3f(-1.0, 0.0, 0.0);
+   glVertex3f(1.0, 0.0, 0.0);
+   glColor3f(0.0, 1.0, 0.0);
+   glVertex3f(0.0, -1.0, 0.0);
+   glColor3f(1.0, 1.0, 0.0); // En modo flat el eje se queda en amarillo, para que se vea en verde y amarillo hay que poner GL_SMOOTH. FLAT coge el último color activado
+   glVertex3f(0.0, 1.0, 0.0);
+   glEnd();
 }
 
-
-void Monigote ()
+void Circle(GLfloat radio, GLfloat cx, GLfloat cy, GLint n, GLenum modo)
 {
-  
- 
-// cara
+   int i;
+   if (modo == GL_LINE)
+      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+   else if (modo == GL_FILL)
+      glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+   else
+      glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+   glBegin(GL_POLYGON);
+   for (i = 0; i < n; i++)
+      glVertex2f(cx + radio * cos(2.0 * M_PI * i / n), cy + radio * sin(2.0 * M_PI * i / n));
+   glEnd();
+}
+
+void Monigote()
+{
+
+   // cara
    glLineWidth(1);
-   glColor3f(1.0,0.8,0.6);
+   glColor3f(1.0, 0.8, 0.6);
    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
    glBegin(GL_POLYGON);
-      glVertex3f(-0.2,0.0,0.0);
-      glVertex3f(0.2,0.0,0.0);
-      glVertex3f(0.2,0.55,0.0);
-      glVertex3f(-0.2,0.55,0.0);
-   glEnd(); 
+   glVertex3f(-0.2, 0.0, 0.0);
+   glVertex3f(0.2, 0.0, 0.0);
+   glVertex3f(0.2, 0.55, 0.0);
+   glVertex3f(-0.2, 0.55, 0.0);
+   glEnd();
 
-   glColor3f(0.0,0.0,0.);
+   glColor3f(0.0, 0.0, 0.);
    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
    glBegin(GL_POLYGON);
-      glVertex3f(-0.2,0.0,0.0);
-      glVertex3f(0.2,0.0,0.0);
-      glVertex3f(0.2,0.55,0.0);
-      glVertex3f(-0.2,0.55,0.0);
-   glEnd(); 
-   
+   glVertex3f(-0.2, 0.0, 0.0);
+   glVertex3f(0.2, 0.0, 0.0);
+   glVertex3f(0.2, 0.55, 0.0);
+   glVertex3f(-0.2, 0.55, 0.0);
+   glEnd();
 }
 
-
-static void Init( )
+static void Init()
 {
 
-   glShadeModel( GL_FLAT); //Si cambiamos a GL_SMOOTH te hace interpolación de colores
+   glShadeModel(GL_FLAT); // Si cambiamos a GL_SMOOTH te hace interpolación de colores
 }
 
-
-static void Reshape( int width, int height )
+static void Reshape(int width, int height)
 {
-    glViewport(0, 0, (GLint)width, (GLint)height); //Se le va pasando el nuevo valor cuando se cambia el tamaño
-    glOrtho (-1.0, 1.0,-1.0, 1.0, -10, 10.0); //Espacio del mundo, los dos primeros x los siguientes la y y los siguientes la z
+   glViewport(0, 0, (GLint)width, (GLint)height); // Se le va pasando el nuevo valor cuando se cambia el tamaño
+   glOrtho(-1.0, 1.0, -1.0, 1.0, -10, 10.0);      // Espacio del mundo, los dos primeros x los siguientes la y y los siguientes la z
 }
 
-static void Display( ) //Es decir, display vuelve a crear lo que había que pintar.
+static void Display() // Es decir, display vuelve a crear lo que había que pintar.
 {
 
-  glClearColor(0.5,0.5,0.5,0.0);
-  glClear( GL_COLOR_BUFFER_BIT ); //Limpiamos el buffer para que no arrastre la imagen anterior
-   
-  
-   
+   glClearColor(0.5, 0.5, 0.5, 0.0);
+   glClear(GL_COLOR_BUFFER_BIT); // Limpiamos el buffer para que no arrastre la imagen anterior
+
    Ejes(6);
    Monigote();
-   
-   glFlush(); 
+   glColor3f(0.55, 0.0, 0.0);
+   Circle(0.05, -0.1, 0.4, 5, GL_FILL);
+   Circle(0.05, 0.1, 0.4, 5, GL_FILL);
+
+   glFlush();
 }
 
-
-static void Keyboard(unsigned char key, int x, int y )
+static void Keyboard(unsigned char key, int x, int y)
 {
- 
-  if (key==27)
+
+   if (key == 27)
       exit(0);
-
 }
 
-
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-   glutInit(&argc,argv);
-   glutInitDisplayMode( GLUT_RGB );
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_RGB);
 
-   glutInitWindowPosition( 20, 100 );
-   glutInitWindowSize(500, 500 );
+   glutInitWindowPosition(20, 100);
+   glutInitWindowSize(500, 500);
    glutCreateWindow("Practica 0 IG");
-
 
    Init();
 
-   glutReshapeFunc(Reshape); //Cambio de tamanio
-   glutDisplayFunc(Display); //Cuando está oculta nuestra pantalla, pinchamos y se vuelve a ver
-   glutKeyboardFunc(Keyboard); //En este caso solo está activa la tecla de escape
-  
-   glutMainLoop( );
+   glutReshapeFunc(Reshape);   // Cambio de tamanio
+   glutDisplayFunc(Display);   // Cuando está oculta nuestra pantalla, pinchamos y se vuelve a ver
+   glutKeyboardFunc(Keyboard); // En este caso solo está activa la tecla de escape
+
+   glutMainLoop();
 
    return 0;
 }
-
